16b08307fa31c0d33c885235783839a6304a2481
347900357 <347900357@qq.com>
2019-12-19 23:44:28 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/jisuanqi/MainActivity.java
+++ app/src/main/java/com/example/jisuanqi/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     SoundPool sp;//声明SoundPool的引用
     HashMap<String, Integer> hm;//声明HashMap来存放声音文件
     int currStaeamId;//当前正播放的streamId
+  //更新----------------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
